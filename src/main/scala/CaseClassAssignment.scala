60f5fa2749d082d8b127e813c3a483add3a8794a
YohanNayanajith <yohannayanajith40@gmail.com>
2021-08-30 18:19:21 +0530

    "Second commit"

--- src/main/scala/CaseClassAssignment.scala
+++ src/main/scala/CaseClassAssignment.scala
@@ -1,16 +1,18 @@
 object CaseClassAssignment {
-  case class Point(x:Int,y:Int){
-    def + :Int = x+y
+  case class Point(a:Int,b:Int){
+    def x:Int = a
+    def y:Int = b
+
+    def +(that:Point):Point  = Point(this.x+that.x,this.y+that.y)
 
     def multi(a:Int,b:Int):Int={
       a*b
     }
-    def move(d:Int):Unit = {
-      println("Move x distance: "+multi(x,d))
-      println("Move x distance: "+multi(y,d))
+    def move(dx:Int,dy:Int):Point = {
+      Point(dx+x,dy+y)
     }
     def distance():Int={
-      x-y
+      this.x-this.y
     }
 
     def invert():Unit={
@@ -19,10 +21,18 @@ object CaseClassAssignment {
   }
 
   def main(args:Array[String]): Unit ={
-    val p1 = Point(10,8)
-    println(p1.+)
-    p1.move(5)
-    println("Differenet between given distance : "+p1.distance())
+    val p1 = Point(10,3)
+    val p2 = Point(5,10)
+    val p3 = p1.move(2,3)
+    val p4 = p1.copy(a=6)
+
+    println(p1)
+    println(p2)
+    println(p3)
+    println(p4)
+    println(p1 == p2)
+    println("Addition of two case classes: "+p1+p2)
+    println("Different between the points: "+p1.distance())
     p1.invert()
   }
 
