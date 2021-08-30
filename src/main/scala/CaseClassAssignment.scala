3b6c03d56306131f35f9ec48ee2d9eb8110f04f4
YohanNayanajith <yohannayanajith40@gmail.com>
2021-08-30 22:32:07 +0530

    "Fourth commit"

--- src/main/scala/CaseClassAssignment.scala
+++ src/main/scala/CaseClassAssignment.scala
@@ -5,15 +5,10 @@ object CaseClassAssignment {
 
     def +(that:Point):Point  = Point(this.x+that.x,this.y+that.y)
 
-    def multi(a:Int,b:Int):Int={
-      a*b
-    }
     def move(dx:Int,dy:Int):Point = {
       Point(dx+x,dy+y)
     }
-    def distance():Int={
-      this.x-this.y
-    }
+    def -(that:Point):Point  = Point(this.x-that.x,this.y-that.y)
 
     def invert():Unit={
       println("Invert X and Y points: "+Point(y,x))
@@ -32,7 +27,7 @@ object CaseClassAssignment {
     println(p4)
     println(p1 == p2)
     println("Addition of two case classes: "+p1.+(p2))
-    println("Different between the points: "+p1.distance())
+    println("Different between the points: "+p1.-(p2))
     p1.invert()
   }
 
